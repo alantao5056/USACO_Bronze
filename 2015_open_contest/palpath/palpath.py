5114c43f2353938f6b0345f9743e49a050ea73f3
alantao5056 <alantao5056@outlook.com>
2020-12-16 19:21:56 -0500

    added tests

--- 2015_open_contest/palpath/palpath.py
+++ 2015_open_contest/palpath/palpath.py
@@ -1,15 +1,16 @@
 def getNumOfPalindrom(field: list) -> int:
-  
+  pass
 
 def main(inputFile: str, outputFile: str):
   palpathInput = open(inputFile, 'r')
   palpathOutput = open(outputFile, 'w')
-  
+
   N = int(palpathInput.readline().strip())
   field = []
-  
+
   for _ in range(N):
     field.append(palpathInput.readline().strip())
-  
-  
-  
+
+  palpathOutput.write(f'{getNumOfPalindrom(field)}\n')
+
+main('palpath.in', 'palpath.out')
