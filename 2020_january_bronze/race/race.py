5d91aa1af072488a12342d950624ff058ee2afde
alantao5056 <alantao5056@outlook.com>
2021-01-14 14:44:19 -0500

    edited

--- 2020_january_bronze/race/race.py
+++ 2020_january_bronze/race/race.py
@@ -1,24 +1,3 @@
-def getSpeedAdd(K, x, curDistance, curSpeed):
-  for i in range(curSpeed, x - 1, -1):
-    curDistance += i
-  if curDistance == K:
-    return 0
-  return 1
-  
-
-def getTime(K, x):
-  count = 0
-  curDistance = 0
-  curSpeed = 0
-
-  while curDistance < K:
-    curSpeed += getSpeedAdd(K, x, curDistance, curSpeed)
-    curDistance += curSpeed
-    count += 1
-
-  return count
-
-
 def main(inputFile: str, outputFile: str):
   raceInput = open(inputFile, 'r')
   raceOutput = open(outputFile, 'w')
