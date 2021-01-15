1f36fa10412f700b7ec4c455d7a7cb4aaa76fcc7
alantao5056 <alantao5056@outlook.com>
2021-01-14 20:33:49 -0500

    working on race

--- 2020_january_bronze/race/race.py
+++ 2020_january_bronze/race/race.py
@@ -1,17 +1,37 @@
 def couldAdd(distanceRan, finishSpeed, speed, distance):
-  while distanceRan < K:
+  if speed == 0:
+    return True
+  speed += 1
+  distanceRan += speed
+  while distanceRan < distance:
     distanceRan += (speed - 1)
+    speed -= 1
     if speed == 0:
       return True
   if speed > finishSpeed:
     return False
   return True
 
+def couldKeepGoing(distanceRan, finishSpeed, speed, distance):
+  distanceRan += speed
+  return couldAdd(distanceRan, finishSpeed, speed, distance)
+
 def getTime(distance, finishSpeed):
   distanceRan = 0
-  runningLog = []
+  curSpeed = 0
+  count = 0
   
-  
+  while distanceRan < distance:
+    if couldAdd(distanceRan, finishSpeed, curSpeed, distance):
+      curSpeed += 1
+      distanceRan += curSpeed    
+    elif couldKeepGoing(distanceRan, finishSpeed, curSpeed, distance):
+      distanceRan += curSpeed
+    else:
+      curSpeed -= 1
+      distanceRan += curSpeed
+    count += 1
+  return count
   
 def main(inputFile: str, outputFile: str):
   raceInput = open(inputFile, 'r')
